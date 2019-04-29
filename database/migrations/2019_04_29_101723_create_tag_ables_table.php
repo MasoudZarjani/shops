08571aeaecda3d602bab485ec099d7836fb2759f
masoud zarjani <masoud.mail.info@gmail.com>
2019-04-29 14:59:25 +0430

    create new table and model

---
+++ database/migrations/2019_04_29_101723_create_tag_ables_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagAblesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tag_ables', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            Relations::pointer($table, 'tag');
+
+            Relations::morph($table, 'tag');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tag_ables');
+    }
+}
